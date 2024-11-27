675a0c7bec348b92e478215c51e7724a7be633b4
Hanan Gershon Cohen <hanang.cohen@gmail.com>
2024-11-28 00:28:59 +0200

    Function "Main" , I started vectors of guards and rocks of their type.

--- src/Manger.cpp
+++ src/Manger.cpp
@@ -18,91 +18,91 @@ using namespace std::chrono_literals;
 
 void Manger::ran()
 {
-    Board board("level01.txt");
+    //Board board("level01.txt");
 
-    board.print();
+    //board.print();
 
-    Location robot_location = board.getRobot_ferst_Location();
-    Robot robot(robot_location);
+    //Location robot_location = board.getRobot_ferst_Location();
+    //Robot robot(robot_location);
 
-    vector <Guard> guards_location = board.get_gard_ferst_location();
-    vector <Bomb> bombs_location;
-    
-    //// תנועת הרובוט
-    int row = robot_location.row;
-    int col = robot_location.col;
-    char** level = board.getLevel();
-    int rowCount = board.getRowCount();
-    int colCount = board.getColCount();
+    //vector <Guard> guards_location = board.get_gard_ferst_location();
+    //vector <Bomb> bombs_location;
+    //
+    ////// תנועת הרובוט
+    //int row = robot_location.row;
+    //int col = robot_location.col;
+    //char** level = board.getLevel();
+    //int rowCount = board.getRowCount();
+    //int colCount = board.getColCount();
 
-    board.print();
+    //board.print();
 
    
-    for (int i = 0; i < 10; i++)
-    {
+    //for (int i = 0; i < 10; i++)
+    //{
 
-        if (_getch() == Keys::SPECIAL_KEY)
-        {
-            int move = _getch();
+    //    if (_getch() == Keys::SPECIAL_KEY)
+    //    {
+    //        int move = _getch();
 
-            // עדכון מיקום חדש
-            int newRow = row, newCol = col;
-            if (move == SpecialKeys::UP && row > 0) newRow = row - 1;
-            else if (move == SpecialKeys::DOWN && row < rowCount - 1) newRow = row + 1;
-            else if (move == SpecialKeys::LEFT && col > 0) newCol = col - 1;
-            else if (move == SpecialKeys::RIGHT && col < colCount - 1) newCol = col + 1;
+    //        // עדכון מיקום חדש
+    //        int newRow = row, newCol = col;
+    //        if (move == SpecialKeys::UP && row > 0) newRow = row - 1;
+    //        else if (move == SpecialKeys::DOWN && row < rowCount - 1) newRow = row + 1;
+    //        else if (move == SpecialKeys::LEFT && col > 0) newCol = col - 1;
+    //        else if (move == SpecialKeys::RIGHT && col < colCount - 1) newCol = col + 1;
 
-            // עדכון הלוח
-            if (level[newRow][newCol] == ' ') {
-                level[row][col] = ' ';
-                level[newRow][newCol] = '/';
-                row = newRow;
-                col = newCol;
-                robot.setLocation(Location(row, col));
-            }
+    //        // עדכון הלוח
+    //        if (level[newRow][newCol] == ' ') {
+    //            level[row][col] = ' ';
+    //            level[newRow][newCol] = '/';
+    //            row = newRow;
+    //            col = newCol;
+    //            robot.setLocation(Location(row, col));
+    //        }
 
-            // הדפסת הלוח מחדש
+    //        // הדפסת הלוח מחדש
 
-           // board.print();
-        }
+    //       // board.print();
+    //    }
 
-        else //its a bomb
-            bombs_location.push_back(Bomb(robot.get_location()));
-
-
-
-        int size = bombs_location.size();
-        for (int i = 0; i < size; i++)
-        {
-            if (bombs_location.at(i).isExploded())
-            {
-                level[bombs_location.at(i).getLocation().row][bombs_location.at(i).getLocation().col] = ' ';
-                bombs_location.erase(bombs_location.begin());
-                i--;
-                size--;
-                continue;
-            }
-
-            else if (bombs_location.at(i).isExploding())
-                level[bombs_location.at(i).getLocation().row][bombs_location.at(i).getLocation().col] = '*';
+    //    else //its a bomb
+    //        bombs_location.push_back(Bomb(robot.get_location()));
 
 
-            else
-                level[bombs_location.at(i).getLocation().row][bombs_location.at(i).getLocation().col] = '0' + bombs_location.at(i).showTime();
 
-            bombs_location.at(i).renwTime();
+    //    int size = bombs_location.size();
+    //    for (int i = 0; i < size; i++)
+    //    {
+    //        if (bombs_location.at(i).isExploded())
+    //        {
+    //            level[bombs_location.at(i).getLocation().row][bombs_location.at(i).getLocation().col] = ' ';
+    //            bombs_location.erase(bombs_location.begin());
+    //            i--;
+    //            size--;
+    //            continue;
+    //        }
 
-        }
+    //        else if (bombs_location.at(i).isExploding())
+    //            level[bombs_location.at(i).getLocation().row][bombs_location.at(i).getLocation().col] = '*';
 
-      //  board.print();
+
+    //        else
+    //            level[bombs_location.at(i).getLocation().row][bombs_location.at(i).getLocation().col] = '0' + bombs_location.at(i).showTime();
+
+    //        bombs_location.at(i).renwTime();
+
+    //    }
+
+    //  //  board.print();
 
 
-        for (int i = 0; i < guards_location.size(); i++)
-        {
-            guards_location[i].move(board);
-            std::this_thread::sleep_for(1000ms);
-        }
-    }
+    //    for (int i = 0; i < guards_location.size(); i++)
+    //    {
+    //        guards_location[i].move(board);
+    //        std::this_thread::sleep_for(1000ms);
+    //    }
+    //}
 
 
    
